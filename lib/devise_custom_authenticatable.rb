ba75642d561483763841668d4877af1b7f00307b
bobzeng <bobzeng@cadence.com>
2015-01-16 05:25:54 -0800

    Restruct the folder hierachy and add option to add new account when first logon
    Restruct the folder hierachy
    Add fuction find_for_custom_authentication to add new account when
    first logon

--- lib/devise_custom_authenticatable.rb
+++ lib/devise_custom_authenticatable.rb
@@ -1,13 +1,19 @@
 # coding: utf-8
 require 'devise'
 
-require 'devise/models/custom_authenticatable'
-require 'devise/strategies/custom_authenticatable'
+require 'devise_custom_authenticatable/model'
+require 'devise_custom_authenticatable/strategy'
 require 'devise_custom_authenticatable/version'
+
+module Devise
+  # Add valid users to database
+  mattr_accessor :custom_create_user
+  @@custom_create_user = false
+end
 
 Devise.add_module(:custom_authenticatable, {
   route: :session,
   strategy: true,
   controller: :sessions,
-  model: 'devise/models/custom_authenticatable'
+  model: 'devise_custom_authenticatable/model'
 })
